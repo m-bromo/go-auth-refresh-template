285618e30093faf0ab259ddd40b55a863df210f9
bromolima <bromolima@gmail.com>
2026-06-23 07:22:15 -0300

    refactor: refactor application structure and move initialization logic to app package

---
+++ internal/app/app.go
@@ -0,0 +1,85 @@
+package app
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/m-bromo/go-auth-template/config"
+	"github.com/m-bromo/go-auth-template/internal/infra/cache"
+	"github.com/m-bromo/go-auth-template/internal/infra/database"
+	"github.com/m-bromo/go-auth-template/internal/infra/database/sqlc"
+	"github.com/m-bromo/go-auth-template/internal/infra/email"
+	"github.com/m-bromo/go-auth-template/internal/repository"
+	"github.com/m-bromo/go-auth-template/internal/service"
+	"github.com/m-bromo/go-auth-template/internal/web/cookie"
+	"github.com/m-bromo/go-auth-template/internal/web/handler"
+	"github.com/m-bromo/go-auth-template/internal/web/middleware"
+	"github.com/m-bromo/go-auth-template/internal/web/routes"
+	"github.com/m-bromo/go-auth-template/internal/web/server"
+	"github.com/redis/go-redis/v9"
+)
+
+type App struct {
+	Server *server.Server
+	DB     *sql.DB
+	Redis  *redis.Client
+}
+
+func New(cfg *config.Config) (*App, error) {
+	db, err := database.NewPostgresConnection(cfg)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	redisClient := cache.NewRedisClient(cfg)
+
+	dependencies := setupDependencies(cfg, db, redisClient)
+
+	srv := server.New()
+
+	routes.SetupRoutes(srv, dependencies)
+
+	return &App{
+		Server: srv,
+		DB:     db,
+		Redis:  redisClient,
+	}, nil
+}
+
+func setupDependencies(cfg *config.Config, db *sql.DB, redisClient *redis.Client) routes.Dependencies {
+	querier := sqlc.New(db)
+	emailSender := email.NewEmailSender(cfg)
+
+	userRepository := repository.NewUserRepository(querier)
+	resetTokenRepository := repository.NewResetTokenRepository(querier)
+	otpRepository := repository.NewOtpRepository(redisClient, cfg)
+	unitOfWork := repository.NewUnitOfWork(cfg, db, querier)
+	refreshTokenRepository := repository.NewRefreshTokenRepository(querier, cfg)
+
+	userService := service.NewUserService(userRepository)
+	jwtService := service.NewJwtService(cfg)
+	refreshTokenService := service.NewRefreshTokenService(cfg, refreshTokenRepository, jwtService)
+	otpService := service.NewOtpService(otpRepository, userRepository, resetTokenRepository, emailSender, cfg)
+	authService := service.NewAuthService(
+		cfg,
+		unitOfWork,
+		userRepository,
+		resetTokenRepository,
+		jwtService,
+		refreshTokenService,
+		otpService,
+	)
+
+	cookieManager := cookie.NewCookieManager(cfg)
+
+	authMiddleware := middleware.NewAuthMiddleware(jwtService)
+
+	authHandler := handler.NewAuthHandler(authService, refreshTokenService, otpService, cookieManager)
+	userHandler := handler.NewUserHandler(userService)
+
+	return routes.Dependencies{
+		AuthMiddleware: authMiddleware,
+		AuthHandler:    authHandler,
+		UserHandler:    userHandler,
+	}
+}
--- main.go
+++ main.go
@@ -6,17 +6,7 @@ import (
 	"log/slog"
 
 	"github.com/m-bromo/go-auth-template/config"
-	"github.com/m-bromo/go-auth-template/internal/infra/cache"
-	"github.com/m-bromo/go-auth-template/internal/infra/database"
-	"github.com/m-bromo/go-auth-template/internal/infra/database/sqlc"
-	"github.com/m-bromo/go-auth-template/internal/infra/email"
-	"github.com/m-bromo/go-auth-template/internal/repository"
-	"github.com/m-bromo/go-auth-template/internal/service"
-	"github.com/m-bromo/go-auth-template/internal/web/cookie"
-	"github.com/m-bromo/go-auth-template/internal/web/handler"
-	"github.com/m-bromo/go-auth-template/internal/web/middleware"
-	"github.com/m-bromo/go-auth-template/internal/web/routes"
-	"github.com/m-bromo/go-auth-template/internal/web/server"
+	"github.com/m-bromo/go-auth-template/internal/app"
 )
 
 func main() {
@@ -27,45 +17,13 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	db, err := database.NewPostgresConnection(cfg)
+	app, err := app.New(cfg)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	defer app.DB.Close()
 
-	srv := server.New()
-	querier := sqlc.New(db)
-	redisClient := cache.NewRedisClient(cfg)
-	emailSender := email.NewEmailSender(cfg)
-	userRepository := repository.NewUserRepository(querier)
-	resetTokenRepository := repository.NewResetTokenRepository(querier)
-	unitOfWork := repository.NewUnitOfWork(cfg, db, querier)
-	otpRepository := repository.NewOtpRepository(redisClient, cfg)
-	refreshTokenRepository := repository.NewRefreshTokenRepository(querier, cfg)
-	userService := service.NewUserService(userRepository)
-	jwtService := service.NewJwtService(cfg)
-	refreshTokenService := service.NewRefreshTokenService(cfg, refreshTokenRepository, jwtService)
-	otpService := service.NewOtpService(otpRepository, userRepository, resetTokenRepository, emailSender, cfg)
-	authService := service.NewAuthService(
-		cfg,
-		unitOfWork,
-		userRepository,
-		resetTokenRepository,
-		jwtService,
-		refreshTokenService,
-		otpService,
-	)
-	cookieManager := cookie.NewCookieManager(cfg)
-	authMiddleware := middleware.NewAuthMiddleware(jwtService)
-	authHandler := handler.NewAuthHandler(authService, refreshTokenService, otpService, cookieManager)
-	userHandler := handler.NewUserHandler(userService)
-
-	routes.SetupRoutes(srv, routes.Dependencies{
-		AuthHandler:    authHandler,
-		UserHandler:    userHandler,
-		AuthMiddleware: authMiddleware,
-	})
-
-	if err := srv.Run(fmt.Sprintf("%s:%s", cfg.API.Host, cfg.API.Port)); err != nil {
+	if err := app.Server.Run(fmt.Sprintf("%s:%s", cfg.API.Host, cfg.API.Port)); err != nil {
 		log.Fatal(err)
 	}
 }
