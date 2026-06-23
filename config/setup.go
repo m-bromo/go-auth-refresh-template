0560c0b4ade2aec429c63c2e91b868d70f30fb66
bromolima <bromolima@gmail.com>
2026-06-23 08:09:25 -0300

    feat: add port listening functionality and configuration handling

--- config/setup.go
+++ config/setup.go
@@ -1,8 +1,17 @@
 package config
 
 import (
+	"errors"
+	"fmt"
+	"net"
+	"strconv"
+
 	"github.com/Netflix/go-env"
 	"github.com/joho/godotenv"
+)
+
+const (
+	MaxPort = 65535
 )
 
 func NewConfig(envPaths ...string) (*Config, error) {
@@ -16,5 +25,39 @@ func NewConfig(envPaths ...string) (*Config, error) {
 		return nil, err
 	}
 
+	if config.IsDevelopment() {
+		port, err := strconv.Atoi(config.API.Port)
+		if err != nil {
+			return nil, err
+		}
+
+		port, err = listenFromPort(config.API.Host, port)
+		if err != nil {
+			return nil, err
+		}
+
+		config.API.Port = strconv.Itoa(port)
+	}
+
 	return &config, nil
+}
+
+func listenFromPort(host string, startPort int) (int, error) {
+	for port := startPort; port <= MaxPort; port++ {
+		addr := net.JoinHostPort(host, strconv.Itoa(port))
+
+		listener, err := net.Listen("tcp", addr)
+		if err == nil {
+			_ = listener.Close()
+			return port, nil
+		}
+
+		var netErr *net.OpError
+		if !errors.As(err, &netErr) {
+			return 0, fmt.Errorf("checking port %d: %w", port, err)
+		}
+	}
+
+	return 0, fmt.Errorf("no available port found from %d", startPort)
+
 }
